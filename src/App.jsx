e6c774fc9963621e837b2f92fa9907799ebf5749
Alperen ATASEVEN <alperenataseven.3@gmail.com>
2024-12-23 06:27:35 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import Homepage from './pages/Homepage'
+import Contact from './pages/Contact'
+
+function App() {
+
+  return (
+    <>
+      <h1>App!</h1>
+      <Navbar />
+      <Homepage />
+      <Contact />
+    </>
+  )
+}
+
+export default App
