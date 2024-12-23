ef093bc33b9e9406780a091068d945b59261bf2c
Alperen ATASEVEN <alperenataseven.3@gmail.com>
2024-12-23 19:35:42 +0300

    Yapı Oluşturuldu

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import { useState } from 'react'
-import Navbar from './components/Navbar'
-import Homepage from './pages/Homepage'
-import Contact from './pages/Contact'
 
 function App() {
 
   return (
     <>
       <h1>App!</h1>
-      <Navbar />
-      <Homepage />
-      <Contact />
     </>
   )
 }
